51f416d0ac3d5724d10b82150bc863a0c3425c02
Burak Çolak <35burakcolak@gmail.com>
2026-01-05 19:47:39 +0300

    Hatalar giderildi

--- backend/src/test/java/com/yurt/project/YurtYonetimSistemiApplicationTest.java
+++ backend/src/test/java/com/yurt/project/YurtYonetimSistemiApplicationTest.java
@@ -3,19 +3,12 @@ package com.yurt.project;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 class YurtYonetimSistemiApplicationTest {
 
     @Test
     void contextLoads() {
-        // Bu metodun içi boş kalsa bile, Spring Context'i ayağa kaldırdığı için
-        // main metodu test edilmiş sayılır.
+        // Sadece Spring Context'in hatasız yüklenip yüklenmediğini kontrol eder.
     }
 
-    @Test
-    void main() {
-        // Main metodunu manuel olarak da çağırarak %100 coverage garantileyelim
-        YurtYonetimSistemiApplication.main(new String[] {});
-    }
 }
